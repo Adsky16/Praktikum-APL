cf4644e8f8b9f769e14ce439d54cbce963cf67de
unknown <adityafatchurohman@gmail.com>
2026-03-14 12:42:30 +0800

    PT3

---
+++ post-test-apl/post-test-apl-3/2509106084-AdityaFatchuRohman-PT-3.cpp
@@ -0,0 +1,227 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cstdlib>
+
+using namespace std;
+
+struct Produk {
+    int id;
+    string nama_barang;
+    string kategori;
+    int stok;
+};
+
+struct Pengguna {
+    string username;
+    string password;
+    string status;
+};
+
+void tampilkanHeader(string statusUser = "", string userAktif = "") {
+    system("cls || clear");
+    cout << "===============================================\n";
+    cout << "       INVENTARIS TOKO KACA & ALUMINIUM        \n";
+    cout << "===============================================\n";
+}
+
+void jeda(string pesan) {
+    cout << "\n " << pesan;
+    cin.ignore();
+    cin.get();
+}
+
+int prosesLogin(Pengguna daftar[], int jml, string &userAktif, string &statusAktif) {
+    string nama, pass;
+    cout << "\n Username: "; cin >> nama;
+    cout << " Password: "; cin >> pass;
+
+    for (int i = 0; i < jml; i++) {
+        if (daftar[i].username == nama && daftar[i].password == pass) {
+            userAktif = nama;
+            statusAktif = daftar[i].status;
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void menuRegistrasi(Pengguna daftar[], int &jml, int maks) {
+    if (jml < maks) {
+        cout << "\n Username Baru: "; cin >> daftar[jml].username;
+        cout << " Password Baru: "; cin >> daftar[jml].password;
+        daftar[jml].status = "user";
+        jml++;
+        cout << "\n Akun berhasil dibuat! Silakan Login.\n";
+    } else {
+        cout << "\n Memori user penuh!\n";
+    }
+    jeda("Tekan Enter untuk melanjutkan.");
+}
+
+void tampilkanStok(Produk daftar[], int jml) {
+    system("cls || clear");
+    cout << "\n+-----+----------------------+---------------+----------+\n";
+    cout << "| ID  | Nama Barang          | Kategori      | Stok     |\n";
+    cout << "+-----+----------------------+---------------+----------+\n";
+    if (jml == 0) {
+        cout << "|                ( Data Masih Kosong )                  |\n";
+    } else {
+        for (int i = 0; i < jml; i++) {
+            cout << "| " << left << setw(4) << daftar[i].id 
+                << "| " << setw(21) << daftar[i].nama_barang 
+                << "| " << setw(14) << daftar[i].kategori 
+                << "| " << setw(9) << daftar[i].stok << "|\n";
+        }
+    }
+    cout << "+-----+----------------------+---------------+----------+\n";
+}
+
+void cariBarang(Produk daftar[], int jml) {
+    system("cls || clear");
+    string cari; cout << "\n Masukkan Kategori: "; cin >> cari;
+    int ditemukan = 0;
+    cout << "\n Hasil Pencarian:\n";
+    for (int i = 0; i < jml; i++) {
+        if (daftar[i].kategori == cari) {
+            cout << " - " << daftar[i].nama_barang << " (ID: " << daftar[i].id << ") Stok: " << daftar[i].stok << endl;
+            ditemukan = 1;
+        }
+    }
+    if (ditemukan == 0) cout << "Kategori tidak ditemukan.\n";
+    jeda("Tekan Enter untuk kembali.");
+}
+
+void tambahBarang(Produk daftar[], int &jml, int &lastId, int maks) {
+    if (jml < maks) {
+        daftar[jml].id = lastId++;
+        cout << "\n Nama Barang: "; cin.ignore(); getline(cin, daftar[jml].nama_barang);
+        cout << " Kategori   : "; getline(cin, daftar[jml].kategori);
+        cout << " Stok       : "; cin >> daftar[jml].stok;
+        jml++;
+        cout << "\n Barang berhasil ditambahkan\n";
+    } else {
+        cout << "\n Gudang penuh\n";
+    }
+    jeda("Tekan Enter untuk kembali.");
+}
+
+void editBarang(Produk daftar[], int jml) {
+    tampilkanStok(daftar, jml);
+    cout << "\n--- MENU EDIT STOK ---";
+    
+    if (jml > 0) {
+        int id_edit; cout << "\nMasukkan ID Barang yang akan diedit: "; cin >> id_edit;
+        int ketemu = 0;
+        for (int i = 0; i < jml; i++) {
+            if (daftar[i].id == id_edit) {
+                cout << " Nama Barang: " << daftar[i].nama_barang << endl;
+                cout << " Masukkan Stok Baru: "; cin >> daftar[i].stok;
+                ketemu = 1;
+                cout << "\n Berhasil diupdate!\n";
+                break;
+            }
+        }
+        if (ketemu == 0) cout << "\nID tidak ditemukan.\n";
+    }
+    jeda("Tekan Enter untuk kembali.");
+}
+
+void hapusBarang(Produk daftar[], int &jml) {
+    tampilkanStok(daftar, jml);
+    cout << "\n--- MENU HAPUS BARANG ---";
+    
+    if (jml > 0) {
+        int id_hapus; cout << "\nMasukkan ID Barang yang akan dihapus: "; cin >> id_hapus;
+        int ketemu = 0;
+        for (int i = 0; i < jml; i++) {
+            if (daftar[i].id == id_hapus) {
+                for (int j = i; j < jml - 1; j++) {
+                    daftar[j] = daftar[j+1];
+                }
+                jml--; 
+                ketemu = 1;
+                cout << "\n Barang berhasil dihapus.\n";
+                break;
+            }
+        }
+        if (ketemu == 0) cout << "\nID tidak ditemukan.\n";
+    }
+    jeda("Tekan Enter untuk kembali.");
+}
+
+int main() {
+    const int MAKS_BARANG = 100;
+    const int MAKS_USER = 10;
+    Produk daftar_produk[MAKS_BARANG];
+    Pengguna daftar_user[MAKS_USER];
+    
+    int jumlah_produk = 0, jumlah_user = 1, id_produk_terakhir = 1;
+    int sedang_login = 0;
+    int aplikasi_berjalan = 1;
+    int percobaan_login = 1;
+    
+    string user_aktif = "", status_aktif = "";
+    int pilihan_utama;
+
+    daftar_user[0] = {"adit", "084", "admin"};
+
+    while (aplikasi_berjalan == 1) {
+        if (sedang_login == 0) {
+            tampilkanHeader();
+            cout << " 1. Login\n 2. Registrasi\n 3. Keluar\n-----------------------------------------------\n Pilih: "; cin >> pilihan_utama;
+
+            if (pilihan_utama == 1) {
+                if (percobaan_login > 3) {
+                    cout << "\n Akses ditolak. Anda telah salah login 3 kali.\n";
+                    aplikasi_berjalan = 0; 
+                    jeda("Program akan ditutup.");
+                } else {
+                    cout << "\n (Percobaan " << percobaan_login << "/3)";
+                    if (prosesLogin(daftar_user, jumlah_user, user_aktif, status_aktif) == 1) {
+                        sedang_login = 1;
+                        percobaan_login = 1;
+                        jeda("Login Berhasil! Tekan Enter.");
+                    } else {
+                        cout << "\nLogin Gagal! Username/Password salah.\n";
+                        if (percobaan_login >= 3) {
+                            cout << "Anda salah 3 kali. Program keluar demi keamanan.\n";
+                            aplikasi_berjalan = 0;
+                        }
+                        percobaan_login++;
+                        jeda("Tekan Enter untuk mencoba lagi.");
+                    }
+                }
+            } else if (pilihan_utama == 2) {
+                menuRegistrasi(daftar_user, jumlah_user, MAKS_USER);
+            } else if (pilihan_utama == 3) {
+                aplikasi_berjalan = 0;
+            }
+        } else {
+            tampilkanHeader(status_aktif, user_aktif);
+            if (status_aktif == "admin") {
+                cout << " 1. Lihat Stok\n 2. Cari Kategori\n 3. Tambah Barang\n 4. Edit Barang\n 5. Hapus Barang\n 6. Logout\n";
+            } else {
+                cout << " 1. Lihat Stok\n 2. Cari Kategori\n 3. Logout\n";
+            }
+            cout << "-----------------------------------------------\n Pilih Menu: "; cin >> pilihan_utama;
+
+            if (pilihan_utama == 1) {
+                tampilkanStok(daftar_produk, jumlah_produk);
+                jeda("Tekan Enter kembali.");
+            }
+            else if (pilihan_utama == 2) cariBarang(daftar_produk, jumlah_produk);
+            else if ((status_aktif == "admin" && pilihan_utama == 6) || (status_aktif != "admin" && pilihan_utama == 3)) {
+                sedang_login = 0;
+                user_aktif = "";
+                jeda("Berhasil Logout. Tekan Enter.");
+            } else if (status_aktif == "admin") {
+                if (pilihan_utama == 3) tambahBarang(daftar_produk, jumlah_produk, id_produk_terakhir, MAKS_BARANG);
+                else if (pilihan_utama == 4) editBarang(daftar_produk, jumlah_produk);
+                else if (pilihan_utama == 5) hapusBarang(daftar_produk, jumlah_produk);
+            }
+        }
+    }
+
+    return 0;
+}
